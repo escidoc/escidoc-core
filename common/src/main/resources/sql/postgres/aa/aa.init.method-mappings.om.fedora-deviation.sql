bc6650fe53d9db1cb51b7d2af002f812d843e454
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2010-12-03 15:51:52 +0000

    add method-mappings
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@39186 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/postgres/aa/aa.init.method-mappings.om.fedora-deviation.sql
+++ common/src/main/resources/sql/postgres/aa/aa.init.method-mappings.om.fedora-deviation.sql
@@ -8,6 +8,12 @@
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-get-datastream-dissimination', 'de.escidoc.core.om.service.interfaces.FedoraRestDeviationHandlerInterface', 'getDatastreamDissemination', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-get-datastream-dissimination', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-get-datastream-dissimination-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-get-datastream-dissimination');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-get-datastream-dissimination-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-get-datastream-dissimination');
   
         /**
          * Fedora Deviation mm - getFedoraDescription
@@ -15,6 +21,12 @@ INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_b
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-get-fedora-description', 'de.escidoc.core.om.service.interfaces.FedoraDescribeDeviationHandlerInterface', 'getFedoraDescription', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-get-fedora-description', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-get-fedora-description-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-get-fedora-description');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-get-fedora-description-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-get-fedora-description');
   
         /**
          * Fedora Deviation mm - export
@@ -22,6 +34,12 @@ INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_b
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-export', 'de.escidoc.core.om.service.interfaces.FedoraRestDeviationHandlerInterface', 'export', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-export', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-export-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-export');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-export-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-export');
   
         /**
          * Fedora Deviation mm - cache
@@ -29,6 +47,12 @@ INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_b
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-cache', 'de.escidoc.core.om.service.interfaces.FedoraRestDeviationHandlerInterface', 'cache', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-cache', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-cache-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-cache');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-cache-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-cache');
   
         /**
          * Fedora Deviation mm - removeFromCache
@@ -36,6 +60,12 @@ INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_b
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-remove-from-cache', 'de.escidoc.core.om.service.interfaces.FedoraRestDeviationHandlerInterface', 'removeFromCache', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-remove-from-cache', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-remove-from-cache-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-remove-from-cache');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-remove-from-cache-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-remove-from-cache');
   
         /**
          * Fedora Deviation mm - replaceInCache
@@ -43,4 +73,10 @@ INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_b
 INSERT INTO aa.method_mappings (id, class_name, method_name, action_name, exec_before, single_resource)
   VALUES ('escidoc:mm-fedora-deviation-replace-in-cache', 'de.escidoc.core.om.service.interfaces.FedoraRestDeviationHandlerInterface', 'replaceInCache', 
   'info:escidoc/names:aa:1.0:action:fedora-deviation-replace-in-cache', true, true);
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-replace-in-cache-1', 'info:escidoc/names:aa:1.0:resource:object-type-new', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-replace-in-cache');
+INSERT INTO aa.invocation_mappings (id, attribute_id, path, position, attribute_type, mapping_type, multi_value, value, method_mapping)
+  VALUES ('escidoc-im-fedora-deviation-replace-in-cache-2', 'info:escidoc/names:aa:1.0:resource:object-type', '', 0, 
+          'http://www.w3.org/2001/XMLSchema#string', 3, false, 'fedora-deviation', 'escidoc:mm-fedora-deviation-replace-in-cache');
   
