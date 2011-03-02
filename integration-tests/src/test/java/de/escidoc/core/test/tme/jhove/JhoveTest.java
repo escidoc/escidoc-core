6a8e12fea7165d7190f038b7232e58949ea4806e
SWA <Steffen.Wagner@fiz-karlsruhe.de>
2011-03-02 14:42:57 +0000

    update testdata version; fix test data access for jhove
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@40772 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- integration-tests/src/test/java/de/escidoc/core/test/tme/jhove/JhoveTest.java
+++ integration-tests/src/test/java/de/escidoc/core/test/tme/jhove/JhoveTest.java
@@ -62,7 +62,7 @@ public class JhoveTest extends JhoveTestBase {
         // prepare item with pdf content
         File f =
             downloadTempFile(new URL(
-                properties.getProperty(PropertiesProvider.TESTDATA_URL) + "/"
+                properties.getProperty(PropertiesProvider.TESTDATA_URL) + "/testDocuments/"
                     + "ges-eSciDoc-article.pdf"));
         URL file = uploadFileToStagingServlet(f, "application/pdf");
 
