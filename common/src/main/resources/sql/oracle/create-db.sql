7cbcd4e2035d2abe5f16b155c52042381b219524
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2011-03-30 07:06:47 +0000

    do more configuration
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@41351 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/oracle/create-db.sql
+++ common/src/main/resources/sql/oracle/create-db.sql
@@ -5,31 +5,46 @@
 /**
  * create eSciDoc DB users and schemas
  */  
-CREATE USER ${escidoc.oracle.database.user} IDENTIFIED BY ${escidoc.oracle.database.password}; 
+CREATE USER ${escidoc.oracle.database.user} 
+IDENTIFIED BY ${escidoc.oracle.database.password}
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace}; 
 
 GRANT CONNECT, create any table, create any index, drop any table, drop any index, select any table, insert any table, update any table, delete any table, create any procedure, UNLIMITED TABLESPACE TO ${escidoc.oracle.database.user};
 
-CREATE USER aa IDENTIFIED BY aa;
+CREATE USER aa IDENTIFIED BY aa
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace};
 
 GRANT UNLIMITED TABLESPACE TO aa;
 
-CREATE USER adm IDENTIFIED BY adm; 
+CREATE USER adm IDENTIFIED BY adm
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO adm;
 
-CREATE USER oai IDENTIFIED BY oai; 
+CREATE USER oai IDENTIFIED BY oai
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO oai;
 
-CREATE USER om IDENTIFIED BY om; 
+CREATE USER om IDENTIFIED BY om
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO om;
 
-CREATE USER sm IDENTIFIED BY sm; 
+CREATE USER sm IDENTIFIED BY sm
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO sm;
 
-CREATE USER st IDENTIFIED BY st;
+CREATE USER st IDENTIFIED BY st
+DEFAULT TABLESPACE ${escidoc.oracle.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.oracle.temp.tablespace};
 
 GRANT UNLIMITED TABLESPACE TO st;
 
