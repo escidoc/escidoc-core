a3ce38ce4a5788e1fabd801a7e46466c7d4f2f96
SWA <Steffen.Wagner@fiz-karlsruhe.de>
2011-04-06 08:52:42 +0000

    fix ingest test
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@41497 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- integration-tests/src/test/java/de/escidoc/core/test/om/ingest/soap/IngestSoapTest.java
+++ integration-tests/src/test/java/de/escidoc/core/test/om/ingest/soap/IngestSoapTest.java
@@ -2,7 +2,7 @@ package de.escidoc.core.test.om.ingest.soap;
 
 import org.junit.Test;
 
-import de.escidoc.core.common.exceptions.remote.application.invalid.InvalidXmlException;
+import de.escidoc.core.common.exceptions.remote.application.invalid.XmlCorruptedException;
 import de.escidoc.core.test.common.client.servlet.Constants;
 import de.escidoc.core.test.om.ingest.IngestAbstractTest;
 
@@ -13,11 +13,11 @@ public class IngestSoapTest extends IngestAbstractTest {
     }
 
     /**
-     * Test unexpected parser exception instead of InvalidXmlException during create (see issue INFR-911).
+     * Test unexpected parser exception instead of XmlCorruptedException during create (see issue INFR-911).
      *
      * @throws Exception Thrown if behavior is not as expected.
      */
-    @Test(expected = InvalidXmlException.class)
+    @Test(expected = XmlCorruptedException.class)
     public void testInvalidXml() throws Exception {
 
         ingest("laber-rababer");
