51fb6f3800f02611a56d733ac8442fe89a7b990b
sche <sche>
2011-04-07 13:33:12 +0000

    removed CREATE/DROP LANGUAGE plpgsql
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@41548 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/postgres/create-db.sql
+++ common/src/main/resources/sql/postgres/create-db.sql
@@ -33,14 +33,3 @@ CREATE DATABASE "${fedora.database.name}" WITH ENCODING='UTF8'
  */ 
 CREATE DATABASE "riTriples" WITH ENCODING='SQL_ASCII' 
 	OWNER="${fedora.database.user}" TEMPLATE=template0;
-
-/**
- * create database scripting language
- */ 
-/* The following command is actually wrong, as it creates the PLPGSQL language 
-   in the wrong database. Instead of the root database, it should be created in
-   the ${escidoc.database.name} database.
-   The installer takes care of this issue by directly executing the necessary
-   commands in database-init.xml */
-CREATE LANGUAGE plpgsql;
-    
