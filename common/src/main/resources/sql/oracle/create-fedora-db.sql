1231ab2799eb3043ba7301459b9527515164d75e
sche <sche>
2011-04-11 15:36:48 +0000

    gave the Fedora Oracle database user some additional grants
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@41597 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/oracle/create-fedora-db.sql
+++ common/src/main/resources/sql/oracle/create-fedora-db.sql
@@ -10,7 +10,7 @@ IDENTIFIED BY ${fedora.database.password}
 DEFAULT TABLESPACE ${fedora.default.tablespace}
 TEMPORARY TABLESPACE ${fedora.temp.tablespace}; 
 
-GRANT CONNECT, create any table, create any index, drop any table, drop any index, select any table, insert any table, update any table, delete any table, UNLIMITED TABLESPACE TO ${fedora.database.user};
+GRANT CONNECT, create any table, create any index, create any sequence, create any trigger, drop any table, drop any index, drop any sequence, drop any trigger, select any table, insert any table, update any table, delete any table, alter any sequence, alter any trigger, UNLIMITED TABLESPACE TO ${fedora.database.user};
 
 /**
  * create triplestore DB users
