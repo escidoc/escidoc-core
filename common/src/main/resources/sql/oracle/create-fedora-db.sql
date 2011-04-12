97871e38ecb9bda2885f10d33b8a2c966a2aa7de
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2011-04-12 06:00:13 +0000

    allow user ritriples to create triggers + sequences
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@41600 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/oracle/create-fedora-db.sql
+++ common/src/main/resources/sql/oracle/create-fedora-db.sql
@@ -20,5 +20,5 @@ IDENTIFIED BY ${triplestore.database.password}
 DEFAULT TABLESPACE ${triplestore.default.tablespace}
 TEMPORARY TABLESPACE ${triplestore.temp.tablespace}; 
 
-GRANT CONNECT, create any table, create any index, drop any table, drop any index, select any table, insert any table, update any table, delete any table, UNLIMITED TABLESPACE TO ${triplestore.database.user};
+GRANT CONNECT, create any table, create any index, create any sequence, create any trigger, drop any table, drop any index, drop any sequence, drop any trigger, select any table, insert any table, update any table, delete any table, alter any sequence, alter any trigger, UNLIMITED TABLESPACE TO ${triplestore.database.user};
  
