f2ebdba9b6176b5de8b4d0465e17e11f0d379403
sche <sche>
2011-05-05 14:52:48 +0000

    property cleanup
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@42028 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/resources/sql/oracle/create-db.sql
+++ common/src/main/resources/sql/oracle/create-db.sql
@@ -7,44 +7,44 @@
  */  
 CREATE USER ${escidoc.database.user} 
 IDENTIFIED BY ${escidoc.database.password}
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace}; 
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace}; 
 
 GRANT CONNECT, create any table, create any index, drop any table, drop any index, select any table, insert any table, update any table, delete any table, create any procedure, analyze any, UNLIMITED TABLESPACE TO ${escidoc.database.user};
 
 CREATE USER aa IDENTIFIED BY aa
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace};
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace};
 
 GRANT UNLIMITED TABLESPACE TO aa;
 
 CREATE USER adm IDENTIFIED BY adm
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace}; 
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO adm;
 
 CREATE USER oai IDENTIFIED BY oai
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace}; 
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO oai;
 
 CREATE USER om IDENTIFIED BY om
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace}; 
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO om;
 
 CREATE USER sm IDENTIFIED BY sm
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace}; 
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace}; 
 
 GRANT UNLIMITED TABLESPACE TO sm;
 
 CREATE USER st IDENTIFIED BY st
-DEFAULT TABLESPACE ${escidoc.default.tablespace}
-TEMPORARY TABLESPACE ${escidoc.temp.tablespace};
+DEFAULT TABLESPACE ${escidoc.database.default.tablespace}
+TEMPORARY TABLESPACE ${escidoc.database.temp.tablespace};
 
 GRANT UNLIMITED TABLESPACE TO st;
 
--- common/src/main/resources/sql/oracle/create-fedora-db.sql
+++ common/src/main/resources/sql/oracle/create-fedora-db.sql
@@ -7,8 +7,8 @@
  */  
 CREATE USER ${fedora.database.user} 
 IDENTIFIED BY ${fedora.database.password}
-DEFAULT TABLESPACE ${fedora.default.tablespace}
-TEMPORARY TABLESPACE ${fedora.temp.tablespace}; 
+DEFAULT TABLESPACE ${fedora.database.default.tablespace}
+TEMPORARY TABLESPACE ${fedora.database.temp.tablespace}; 
 
 GRANT CONNECT, create any table, create any index, create any sequence, create any trigger, drop any table, drop any index, drop any sequence, drop any trigger, select any table, insert any table, update any table, delete any table, alter any sequence, alter any trigger, UNLIMITED TABLESPACE TO ${fedora.database.user};
 
@@ -17,8 +17,8 @@ GRANT CONNECT, create any table, create any index, create any sequence, create a
  */  
 CREATE USER ${triplestore.database.user} 
 IDENTIFIED BY ${triplestore.database.password}
-DEFAULT TABLESPACE ${triplestore.default.tablespace}
-TEMPORARY TABLESPACE ${triplestore.temp.tablespace}; 
+DEFAULT TABLESPACE ${triplestore.database.default.tablespace}
+TEMPORARY TABLESPACE ${triplestore.database.temp.tablespace}; 
 
 GRANT CONNECT, create any table, create any index, create any sequence, create any trigger, drop any table, drop any index, drop any sequence, drop any trigger, select any table, insert any table, update any table, delete any table, alter any sequence, alter any trigger, UNLIMITED TABLESPACE TO ${triplestore.database.user};
  
