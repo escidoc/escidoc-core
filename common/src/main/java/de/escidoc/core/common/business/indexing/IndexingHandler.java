7ac201bdacd52e3f7f3f14f45816efbb43c9c5f7
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2011-07-26 14:25:03 +0000

    make variable final
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCore/1.3/eSciDocCore/@43687 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- common/src/main/java/de/escidoc/core/common/business/indexing/IndexingHandler.java
+++ common/src/main/java/de/escidoc/core/common/business/indexing/IndexingHandler.java
@@ -516,8 +516,9 @@ public class IndexingHandler implements ResourceListener {
      * @throws de.escidoc.core.common.exceptions.system.SystemException
      */
     private int checkPrerequisites(
-        String xml, final Map<String, Object> parameters, final String resource, Document domObject)
+        final String xml, final Map<String, Object> parameters, final String resource, Document domObject)
         throws SystemException {
+        String thisXml = xml;
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("prerequisites is " + parameters.get("prerequisites"));
         }
@@ -531,17 +532,17 @@ public class IndexingHandler implements ResourceListener {
                 return Constants.DO_UPDATE;
             }
             else {
-                if (xml == null) {
+                if (thisXml == null) {
                     if (LOGGER.isDebugEnabled()) {
                         LOGGER.debug("xml is null, requesting it from cache");
                     }
-                    xml = indexingCacheHandler.retrieveObjectFromCache(resource);
+                    thisXml = indexingCacheHandler.retrieveObjectFromCache(resource);
                 }
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("xml is: " + xml);
+                    LOGGER.debug("xml is: " + thisXml);
                 }
                 if (domObject == null) {
-                    domObject = getXmlAsDocument(xml);
+                    domObject = getXmlAsDocument(thisXml);
                 }
                 if (prerequisites.get("indexingPrerequisiteXpath") != null) {
                     final Node updateNode =
