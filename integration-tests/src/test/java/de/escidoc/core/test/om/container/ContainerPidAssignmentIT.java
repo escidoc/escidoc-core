d7ad8e7d56cdd9557524206ce69bd152e36e0644
SWA <Steffen.Wagner@fiz-karlsruhe.de>
2011-09-02 07:13:27 +0000

    fix compilation
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@44283 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- integration-tests/src/test/java/de/escidoc/core/test/om/container/ContainerPidAssignmentIT.java
+++ integration-tests/src/test/java/de/escidoc/core/test/om/container/ContainerPidAssignmentIT.java
@@ -919,11 +919,11 @@ public class ContainerPidAssignmentIT extends ContainerTestBase {
     @Test
     public void testPidParameter02() throws Exception {
 
-        String pidToRegister = "hdl:testPrefix/" + containerId;
         String containerXml = this.theContainerXml;
         Document containerDoc = EscidocAbstractTest.getDocument(containerXml);
         String containerId = getObjidValue(containerDoc);
         DateTime lmd = getLastModificationDateValue2(containerDoc);
+        String pidToRegister = "hdl:testPrefix/" + containerId;
 
         AssignParam assignPidParam = new AssignParam();
         assignPidParam.setPid(pidToRegister);
