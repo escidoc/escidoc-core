a6df40f389d5422919ddd407e127c0fd4b604911
SWA <Steffen.Wagner@fiz-karlsruhe.de>
2011-10-15 14:28:49 +0000

    calling rest template
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCore/1.4/eSciDocCore@44598 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- integration-tests/src/test/java/de/escidoc/core/test/om/container/ContainerContentRelationsIT.java
+++ integration-tests/src/test/java/de/escidoc/core/test/om/container/ContainerContentRelationsIT.java
@@ -509,7 +509,7 @@ public class ContainerContentRelationsIT extends ContainerTestBase {
     @Test
     public void testAddExistingInvalidRelation() throws Exception {
 
-        String xmlContainer = getTemplateAsString(TEMPLATE_CONTAINER_PATH, "create_container_WithoutMembers_v1.1.xml");
+        String xmlContainer = getTemplateAsString(TEMPLATE_CONTAINER_PATH + "/rest", "create_container_WithoutMembers_v1.1.xml");
 
         String xml = create(xmlContainer);
 
