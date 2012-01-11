ba62f982b8fc0a47827cf7b0e1382718f334dda1
SWA <Steffen.Wagner@fiz-karlsruhe.de>
2012-01-11 17:01:44 +0000

    add cmm cxf skeleton
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@45129 5c398c58-3d0a-0410-a38c-d0825f6816e0

---
+++ cmm-rest-impl/src/main/java/org/escidoc/core/cmm/internal/ContentModelRestServiceImpl.java
@@ -0,0 +1,71 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the Common Development and Distribution License, Version 1.0
+ * only (the "License"). You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE or http://www.escidoc.de/license. See the License
+ * for the specific language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each file and include the License file at
+ * license/ESCIDOC.LICENSE. If applicable, add the following below this CDDL HEADER, with the fields enclosed by
+ * brackets "[]" replaced with your own identifying information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ * Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft fuer wissenschaftlich-technische Information mbH
+ * and Max-Planck-Gesellschaft zur Foerderung der Wissenschaft e.V. All rights reserved. Use is subject to license
+ * terms.
+ */
+
+package org.escidoc.core.cmm.internal;
+
+import org.escidoc.core.domain.ResultTO;
+import org.escidoc.core.domain.content.relation.ContentRelationPropertiesTO;
+import org.escidoc.core.domain.content.relation.ContentRelationTO;
+import org.escidoc.core.domain.metadatarecords.MdRecordTO;
+import org.escidoc.core.domain.metadatarecords.MdRecordsTO;
+import org.escidoc.core.domain.service.ServiceUtility;
+import org.escidoc.core.domain.taskparam.StatusTaskParamTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import de.escidoc.core.common.exceptions.application.invalid.InvalidContentException;
+import de.escidoc.core.common.exceptions.application.invalid.InvalidStatusException;
+import de.escidoc.core.common.exceptions.application.invalid.InvalidXmlException;
+import de.escidoc.core.common.exceptions.application.invalid.XmlCorruptedException;
+import de.escidoc.core.common.exceptions.application.missing.MissingAttributeValueException;
+import de.escidoc.core.common.exceptions.application.missing.MissingMethodParameterException;
+import de.escidoc.core.common.exceptions.application.notfound.ContentRelationNotFoundException;
+import de.escidoc.core.common.exceptions.application.notfound.MdRecordNotFoundException;
+import de.escidoc.core.common.exceptions.application.notfound.ReferencedResourceNotFoundException;
+import de.escidoc.core.common.exceptions.application.notfound.RelationPredicateNotFoundException;
+import de.escidoc.core.common.exceptions.application.security.AuthenticationException;
+import de.escidoc.core.common.exceptions.application.security.AuthorizationException;
+import de.escidoc.core.common.exceptions.application.violated.LockingException;
+import de.escidoc.core.common.exceptions.application.violated.OptimisticLockingException;
+import de.escidoc.core.common.exceptions.application.violated.PidAlreadyAssignedException;
+import de.escidoc.core.common.exceptions.system.SystemException;
+import org.escidoc.core.cmm.ContentModelRestService;
+import de.escidoc.core.cmm.service.interfaces.ContentModelHandlerInterface;
+
+/**
+ * 
+ * @author SWA
+ * 
+ */
+public class ContentModelRestServiceImpl implements ContentModelRestService {
+
+    @Autowired
+    @Qualifier("service.ContentModelHandler")
+    private ContentModelHandlerInterface contentModelHandler;
+
+    /**
+     * 
+     */
+    protected ContentModelRestServiceImpl() {
+
+    }
+
+}
