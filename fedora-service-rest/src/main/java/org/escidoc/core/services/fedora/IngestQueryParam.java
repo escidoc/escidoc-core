ce10e5c109affefc06a46ffa872c270ee634e58b
mk0 <marko.voss@fiz-karlsruhe.de>
2012-02-15 11:45:06 +0000

    Fixed version number in comment.
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@45445 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- fedora-service-rest/src/main/java/org/escidoc/core/services/fedora/IngestQueryParam.java
+++ fedora-service-rest/src/main/java/org/escidoc/core/services/fedora/IngestQueryParam.java
@@ -58,11 +58,11 @@ public final class IngestQueryParam {
     }
 
     /**
-     * TODO: Remove deprecation when fedora version 1.5 is being used.
+     * TODO: Remove deprecation when fedora version 3.5 is being used.
      *
      * @param ownerId
      * @deprecated If you need to use this method, please communicate this with the team, because this requires us to
-     * update to fedora version 1.5.
+     * update to fedora version 3.5.
      * @see <a href="https://jira.duraspace.org/browse/FCREPO-963">FCREPO-963</a> for details.
      */
     @Deprecated
