cd0377d547fb542990a6e09936a385f45b09abb0
mk0 <marko.voss@fiz-karlsruhe.de>
2012-02-24 13:15:42 +0000

    Missing checkin
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/eSciDocCore@45481 5c398c58-3d0a-0410-a38c-d0825f6816e0

---
+++ context-rest-spec/src/main/java/de/escidoc/core/context/param/CreateQueryParam.java
@@ -0,0 +1,10 @@
+package de.escidoc.core.context.param;
+
+import net.sf.oval.guard.Guarded;
+
+/**
+ * @author Marko Voss (marko.voss@fiz-karlsruhe.de)
+ */
+@Guarded(applyFieldConstraintsToConstructors = true, applyFieldConstraintsToSetters = true, assertParametersNotNull = false, checkInvariants = true, inspectInterfaces = true)
+public class CreateQueryParam {
+}
