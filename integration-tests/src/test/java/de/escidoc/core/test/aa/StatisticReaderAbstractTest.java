ecb4efbea114448243bfdea1e9a18dc9b2dd089f
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2012-04-24 06:25:39 +0000

    use interface-method for preprocessing, not jmx-console
    git-svn-id: http://escidev5.fiz-karlsruhe.de:8888/prototype/branches/eSciDocCore/1.3/eSciDocCore/@45640 5c398c58-3d0a-0410-a38c-d0825f6816e0

--- integration-tests/src/test/java/de/escidoc/core/test/aa/StatisticReaderAbstractTest.java
+++ integration-tests/src/test/java/de/escidoc/core/test/aa/StatisticReaderAbstractTest.java
@@ -28,18 +28,17 @@
  */
 package de.escidoc.core.test.aa;
 
-import de.escidoc.core.test.common.client.servlet.Constants;
-import de.escidoc.core.test.common.client.servlet.HttpHelper;
-import de.escidoc.core.test.security.client.PWCallback;
-import org.apache.http.HttpResponse;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
+import java.util.Date;
+
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import de.escidoc.core.test.EscidocRestSoapTestBase;
+import de.escidoc.core.test.common.client.servlet.Constants;
+import de.escidoc.core.test.security.client.PWCallback;
 
 /**
  * Test suite for testing access-rights to statistic-reports.
@@ -49,10 +48,6 @@ import java.util.regex.Pattern;
 public class StatisticReaderAbstractTest extends GrantTestBase {
 
     private static final String HANDLE = PWCallback.TEST_HANDLE;
-
-    private static final String LOGINNAME = HANDLE;
-
-    private static final String PASSWORD = PWCallback.PASSWORD;
 
     protected static String grantCreationUserOrGroupId = null;
 
@@ -84,14 +79,6 @@ public class StatisticReaderAbstractTest extends GrantTestBase {
     private static final String STATISTIC_DATA_TEMPLATE_NAME = "escidoc_statistic_data1.xml";
 
     private static final String REPORT_PARAMETERS_TEMPLATE_NAME = "escidoc_report_parameters0.xml";
-
-    private static final String PREPROCESSING_URL =
-        "jmx-console/HtmlAdaptor?action=invokeOp" + "&name=eSciDocCore%3Aname%3DStatisticPreprocessorService"
-            + "&methodIndex=${methodIndex}&arg0=";
-
-    private static final String STATISTIC_PREPROCESSR_METHOD_INDEX = "0";
-
-    private static final Pattern METHOD_INDEX_PATTERN = Pattern.compile("\\$\\{methodIndex\\}");
 
     /**
      * The constructor.
@@ -179,43 +166,27 @@ public class StatisticReaderAbstractTest extends GrantTestBase {
         }
 
         // trigger preprocessing of statistic_data
-        triggerPreprocessing(STATISTIC_PREPROCESSR_METHOD_INDEX);
+        String now = new DateTime(new Date().getTime()).toString("yyyy-MM-dd");
+        triggerPreprocessing(aggregationDefinitionId, now);
+        triggerPreprocessing(newAggregationDefinitionId, now);
 
     }
 
     /**
-     * triggers preprocessing.
+     * triggers preprocessing via framework-interface.
      *
-     * @param methodIndex methodIndex
+     * @param aggrDefinitionId aggrDefinitionId
+     * @param date             date
      * @throws Exception If anything fails.
      */
-    private void triggerPreprocessing(final String methodIndex) throws Exception {
-        String urlParameters = PREPROCESSING_URL + System.currentTimeMillis();
-
-        Matcher methodIndexMatcher = METHOD_INDEX_PATTERN.matcher(urlParameters);
-        urlParameters = methodIndexMatcher.replaceAll(methodIndex);
-
-        String httpUrl =
-            HttpHelper.createUrl(Constants.PROTOCOL, Constants.HOST_PORT, Constants.ESCIDOC_BASE_URI + urlParameters);
-        long time = System.currentTimeMillis();
-        HttpResponse httpRes =
-            HttpHelper.executeHttpRequest(null, Constants.HTTP_METHOD_GET, httpUrl, null, null, null);
-        String response = EntityUtils.toString(httpRes.getEntity(), HTTP.UTF_8);
-        httpRes.getEntity().consumeContent();
-        response = " preprocessing needed " + (System.currentTimeMillis() - time) + response;
-        try {
-            assertMatches("String does not match es expected. " + response,
-                "Operation completed successfully without a return value", response);
-
-        }
-        catch (final AssertionError e) {
-            if (methodIndex.equals(STATISTIC_PREPROCESSR_METHOD_INDEX)) {
-                triggerPreprocessing("1");
-            }
-            else {
-                throw e;
-            }
-        }
+    private void triggerPreprocessing(final String aggrDefinitionId, final String date) throws Exception {
+        String preprocessingInformationXml =
+            EscidocRestSoapTestBase.getTemplateAsString(TEMPLATE_PREPROCESSING_INFO_PATH,
+                "escidoc_preprocessing_information1.xml");
+        Document doc = EscidocRestSoapTestBase.getDocument(preprocessingInformationXml);
+        substitute(doc, "/preprocessing-information/start-date", date);
+        substitute(doc, "/preprocessing-information/end-date", date);
+        preprocessingClient.preprocess(aggrDefinitionId, toString(doc, false));
     }
 
     /**
