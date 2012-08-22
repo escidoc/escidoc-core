f10bf7808498671913167707163d7bb9cfa8b415
fasseg <frank.asseg@googlemail.com>
2012-08-22 16:36:09 +0200

    added scape-dto dependency and Scapemarshaller usage to post data

--- common/src/main/java/de/escidoc/core/common/servlet/ScapeIngestServlet.java
+++ common/src/main/java/de/escidoc/core/common/servlet/ScapeIngestServlet.java
@@ -7,19 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ScapeIngestServlet extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
+import eu.scapeproject.model.IntellectualEntity;
+import eu.scapeproject.model.mets.SCAPEMarshaller;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		resp.setContentType("text/plain");
-		resp.getOutputStream().write("scapeage".getBytes());
-		resp.getOutputStream().close();
-		resp.flushBuffer();
-	}}
+public class ScapeIngestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        IntellectualEntity entity;
+        try {
+            entity = SCAPEMarshaller.getInstance().deserialize(IntellectualEntity.class, req.getInputStream());
+        }
+        catch (Exception e) {
+            throw new IOException("unable to deserialize intellectual entity");
+        }
+        resp.setContentType("text/plain");
+        resp.getOutputStream().write(entity.getIdentifier().getValue().getBytes());
+        resp.getOutputStream().flush();
+        resp.getOutputStream().close();
+        resp.flushBuffer();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getOutputStream().write("scapeage".getBytes());
+        resp.getOutputStream().flush();
+        resp.getOutputStream().close();
+        resp.flushBuffer();
+    }
+}
