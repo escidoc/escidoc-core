6c077140613e9b0b03e58708ed88c6f7a332e29b
fasseg <frank.asseg@googlemail.com>
2012-09-12 16:58:16 +0200

    added escidoc-ijc dependency, saving item

--- om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
+++ om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
@@ -2,23 +2,32 @@ package de.escidoc.core.om.service;
 
 import java.io.ByteArrayInputStream;
 
-import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import de.escidoc.core.common.exceptions.EscidocException;
 import de.escidoc.core.common.exceptions.scape.ScapeException;
-import de.escidoc.core.om.business.fedora.item.FedoraItemHandler;
+import de.escidoc.core.common.jibx.Marshaller;
+import de.escidoc.core.common.jibx.MarshallerFactory;
+import de.escidoc.core.resources.common.MetadataRecord;
+import de.escidoc.core.resources.common.MetadataRecords;
+import de.escidoc.core.resources.om.item.Item;
+import de.escidoc.core.resources.om.item.ItemProperties;
 import eu.scapeproject.model.IntellectualEntity;
 import eu.scapeproject.model.mets.SCAPEMarshaller;
 
 @Service("service.ScapeIngestHandler")
 public class ScapeIngestHandler implements de.escidoc.core.om.service.interfaces.ScapeIngestHandlerInterface {
     private static final Logger LOGGER = LoggerFactory.getLogger(ScapeIngestHandler.class);
+
+    private static final DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
 
     @Autowired
     @Qualifier("service.ItemHandler")
@@ -30,9 +39,22 @@ public class ScapeIngestHandler implements de.escidoc.core.om.service.interfaces
             IntellectualEntity entity =
                 SCAPEMarshaller.getInstance().deserialize(IntellectualEntity.class,
                     new ByteArrayInputStream(xml.getBytes()));
+            Document doc = domFactory.newDocumentBuilder().parse(new ByteArrayInputStream(xml.getBytes()));
+            Item item = new Item();
+            ItemProperties itemProps = new ItemProperties();
+            MetadataRecords mds = new MetadataRecords();
+            MetadataRecord dc = new MetadataRecord("dublin-core");
+            dc.setMdType("DC");
+            Element dcElement = doc.getElementById(entity.getDescriptive().getId());
+            dc.setContent(dcElement);
+            mds.add(dc);
+            item.setProperties(itemProps);
+            item.setMetadataRecords(mds);
+            Marshaller<Item> itemMarshaller = MarshallerFactory.getInstance().getMarshaller(Item.class);
+            itemHandler.create(itemMarshaller.marshalDocument(item));
             return entity.getIdentifier().getValue() + "\n";
         }
-        catch (JAXBException e) {
+        catch (Exception e) {
             throw new ScapeException(e.getLocalizedMessage(), e);
         }
     }
