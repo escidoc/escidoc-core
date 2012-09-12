d2a63e97c365ec9a0ca8104a78e207182b8b5a10
fasseg <frank.asseg@googlemail.com>
2012-09-12 13:18:14 +0200

    fixed servlet handling to use EScidocServlet

--- om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
+++ om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
@@ -1,9 +1,24 @@
 package de.escidoc.core.om.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import de.escidoc.core.om.business.fedora.item.FedoraItemHandler;
+
+@Service("service.scapeIngestHandler")
 public class ScapeIngestHandler implements de.escidoc.core.om.service.interfaces.ScapeIngestHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScapeIngestHandler.class);
+
+    @Autowired
+    @Qualifier("service.ItemHandler")
+    private ItemHandler itemHandler;
+
     @Override
     public String ingestIntellectualEntity(String xml) {
-        // TODO Auto-generated method stub
+        LOGGER.debug("ingesting entity:\n" + xml + "\n");
         return null;
     }
 }
