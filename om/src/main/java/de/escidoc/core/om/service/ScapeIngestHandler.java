97cc681c3d22eb5a1882fa87b70786e7664229ef
fasseg <frank.asseg@googlemail.com>
2012-09-17 15:18:02 +0200

    added checking for context filling dc terms to items

--- om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
+++ om/src/main/java/de/escidoc/core/om/service/ScapeIngestHandler.java
@@ -1,6 +1,7 @@
 package de.escidoc.core.om.service;
 
 import java.io.ByteArrayInputStream;
+import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -15,12 +16,16 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import de.escidoc.core.client.interfaces.handler.OrganizationalUnitHandler;
 import de.escidoc.core.common.exceptions.EscidocException;
 import de.escidoc.core.common.exceptions.scape.ScapeException;
 import de.escidoc.core.common.jibx.Marshaller;
 import de.escidoc.core.common.jibx.MarshallerFactory;
 import de.escidoc.core.resources.common.MetadataRecord;
 import de.escidoc.core.resources.common.MetadataRecords;
+import de.escidoc.core.resources.common.properties.PublicStatus;
+import de.escidoc.core.resources.om.context.Context;
+import de.escidoc.core.resources.om.context.ContextProperties;
 import de.escidoc.core.resources.om.item.Item;
 import de.escidoc.core.resources.om.item.ItemProperties;
 import eu.scapeproject.model.IntellectualEntity;
@@ -34,38 +39,90 @@ public class ScapeIngestHandler implements de.escidoc.core.om.service.interfaces
     @Qualifier("service.ItemHandler")
     private ItemHandler itemHandler;
 
+    @Autowired
+    @Qualifier("service.ContextHandler")
+    private ContextHandler contextHandler;
+
+    private Context scapeContext;
+
     @Override
     public String ingestIntellectualEntity(String xml) throws EscidocException {
         try {
+            checkScapeContext();
+
             IntellectualEntity entity =
                 SCAPEMarshaller.getInstance().deserialize(IntellectualEntity.class,
                     new ByteArrayInputStream(xml.getBytes()));
+            // parse the METS xml dom
+            // since we need metadata as org.w3c.Element
             DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
             Document doc = domFactory.newDocumentBuilder().parse(new ByteArrayInputStream(xml.getBytes()));
+
+            // create a new escidoc item using the ijc library
             Item item = new Item();
-            ItemProperties itemProps = new ItemProperties();
+
+            // prepare the dublin core metadata
             MetadataRecords mds = new MetadataRecords();
+            mds.setLastModificationDate(new DateTime());
             MetadataRecord dc = new MetadataRecord("dublin-core");
             dc.setMdType("DC");
             NodeList nodes = doc.getElementsByTagName("mets:dmdSec");
             for (int i = 0; i < nodes.getLength(); i++) {
                 Node n = nodes.item(i);
                 if (entity.getDescriptive().getId().equals(n.getAttributes().getNamedItem("ID").getNodeValue())) {
-                    Element dcElement = (Element) n;
+                    Document dcDoc = domFactory.newDocumentBuilder().newDocument();
+                    Element dcElement = dcDoc.createElement("dublin-core");
+                    dcElement.setAttribute("xmlns:dc", "http://purl.org/dc/elements/1.1/");
+                    NodeList origDCElements =
+                        (NodeList) n
+                            .getChildNodes().item(1).getChildNodes().item(1).getChildNodes().item(1).getChildNodes();
+                    for (int j = 1; j < origDCElements.getLength(); j++) {
+                        dcElement.appendChild(dcDoc.adoptNode(origDCElements.item(j).cloneNode(true)));
+                    }
                     dc.setContent(dcElement);
                     mds.add(dc);
                 }
             }
+
+            // set the properties of the item using the intellectual entities data
+            ItemProperties itemProps = new ItemProperties();
             item.setProperties(itemProps);
             item.setMetadataRecords(mds);
             item.setLastModificationDate(new DateTime());
+
+            // and use ijc's marshaller to create escidoc xml
             Marshaller<Item> itemMarshaller = MarshallerFactory.getInstance().getMarshaller(Item.class);
+
+            // and finally use escidoc's item handler to save the generated xml data
+            itemHandler.create(itemMarshaller.marshalDocument(item));
             return itemMarshaller.marshalDocument(item) + "\n";
-            //			itemHandler.create(itemMarshaller.marshalDocument(item));
-            //			return entity.getIdentifier().getValue() + "\n";
+            // return entity.getIdentifier().getValue() + "\n";
         }
         catch (Exception e) {
             throw new ScapeException(e.getLocalizedMessage(), e);
+        }
+    }
+
+    private void checkScapeContext() throws EscidocException {
+        try {
+            if (scapeContext == null) {
+                Marshaller<Context> contextMarshaller = MarshallerFactory.getInstance().getMarshaller(Context.class);
+                scapeContext = contextMarshaller.unmarshalDocument(contextHandler.retrieve("scape-context"));
+                if (scapeContext == null) {
+                    ContextProperties props = new ContextProperties();
+                    props.setCreationDate(new DateTime());
+                    props.setDescription("a context for SCAPE items");
+                    props.setPublicStatus(PublicStatus.RELEASED);
+                    props.setName("SCAPE context");
+                    scapeContext = new Context();
+                    scapeContext.setLastModificationDate(new DateTime());
+                    scapeContext.setProperties(props);
+                    contextHandler.create(contextMarshaller.marshalDocument(scapeContext));
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new ScapeException(e.getMessage(), e);
         }
     }
 }
